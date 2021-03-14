c8e8faea96c161051d101664575eb7f0a943f912
Philip Mørch <mrphil2105@gmail.com>
2021-03-14 18:45:26 +0100

    Add program startup capabilities

---
+++ Src/AvaloniaApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace Chatter.AvaloniaApp
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args, ShutdownMode.OnMainWindowClose);
+        }
+
+        private static AppBuilder BuildAvaloniaApp()
+        {
+            return AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToTrace();
+        }
+    }
+}
