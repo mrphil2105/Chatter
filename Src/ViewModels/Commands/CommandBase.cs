2a40cf67ba741a94795469571f80f30236420140
Philip Mørch <mrphil2105@gmail.com>
2021-03-15 08:40:23 +0100

    Add base class for commands

---
+++ Src/ViewModels/Commands/CommandBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Chatter.ViewModels.Commands
+{
+    public abstract class CommandBase : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        bool ICommand.CanExecute(object? parameter)
+        {
+            return CanExecute();
+        }
+
+        void ICommand.Execute(object? parameter)
+        {
+            Execute();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected abstract bool CanExecute();
+
+        protected abstract void Execute();
+    }
+}
