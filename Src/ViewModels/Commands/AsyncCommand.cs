c411210a441d5bfb2835c96e53380524d4c1537e
Philip Mørch <mrphil2105@gmail.com>
2021-03-15 08:40:39 +0100

    Add relay and async commands

---
+++ Src/ViewModels/Commands/AsyncCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Chatter.ViewModels.Commands
+{
+    public class AsyncCommand : CommandBase
+    {
+        private readonly Func<bool>? _canExecuteFunc;
+        private readonly Func<Task> _executeFunc;
+
+        private bool _isExecuting;
+
+        public AsyncCommand(Func<Task> executeFunc, Func<bool>? canExecuteFunc = null)
+        {
+            _canExecuteFunc = canExecuteFunc;
+            _executeFunc = executeFunc;
+        }
+
+        protected override bool CanExecute()
+        {
+            return !_isExecuting && (_canExecuteFunc?.Invoke() ?? true);
+        }
+
+        protected override async void Execute()
+        {
+            try
+            {
+                _isExecuting = true;
+                RaiseCanExecuteChanged();
+
+                await _executeFunc();
+            }
+            finally
+            {
+                _isExecuting = false;
+                RaiseCanExecuteChanged();
+            }
+        }
+    }
+}
---
+++ Src/ViewModels/Commands/RelayCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Chatter.ViewModels.Commands
+{
+    public class RelayCommand : CommandBase
+    {
+        private readonly Func<bool>? _canExecuteFunc;
+        private readonly Action _executeAction;
+
+        public RelayCommand(Action executeAction, Func<bool>? canExecuteFunc = null)
+        {
+            _canExecuteFunc = canExecuteFunc;
+            _executeAction = executeAction;
+        }
+
+        protected override bool CanExecute()
+        {
+            return _canExecuteFunc?.Invoke() ?? true;
+        }
+
+        protected override void Execute()
+        {
+            _executeAction();
+        }
+    }
+}
