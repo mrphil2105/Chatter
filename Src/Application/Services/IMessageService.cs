580b2100f4dd4809a4b2e6b97b8ee93b1dd2a8ee
Philip Mørch <mrphil2105@gmail.com>
2021-03-16 16:54:51 +0100

    Create message service interface

---
+++ Src/Application/Services/IMessageService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Chatter.Application.Services
+{
+    public interface IMessageService
+    {
+        event EventHandler<MessageReceivedEventArgs> MessageReceived;
+
+        Task SendMessageAsync(string message);
+    }
+}
---
+++ Src/Application/Services/MessageReceivedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Chatter.Application.Services
+{
+    public class MessageReceivedEventArgs : EventArgs
+    {
+        public MessageReceivedEventArgs(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; }
+    }
+}
