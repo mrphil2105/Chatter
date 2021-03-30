186ce72b29efd2d7bb83832683fcf1715dd6ad63
Philip Mørch <mrphil2105@gmail.com>
2021-03-30 20:50:38 +0200

    Implement IDispatcher as wrapper around Avalonia dispatcher

---
+++ Src/AvaloniaApp/Threading/DispatcherWrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Avalonia.Threading;
+using IDispatcher = Chatter.ViewModels.Abstract.IDispatcher;
+
+namespace Chatter.AvaloniaApp.Threading
+{
+    public class DispatcherWrapper : IDispatcher
+    {
+        private readonly Dispatcher _dispatcher;
+
+        public DispatcherWrapper(Dispatcher dispatcher)
+        {
+            _dispatcher = dispatcher ?? throw new ArgumentNullException(nameof(dispatcher));
+        }
+
+        public Task InvokeAsync(Action action)
+        {
+            return _dispatcher.InvokeAsync(action);
+        }
+    }
+}
