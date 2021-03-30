2add42fc66b8710f4cfcdb34bf35378ee37611a6
Philip Mørch <mrphil2105@gmail.com>
2021-03-30 20:48:46 +0200

    Create dispatcher and view manager interfaces

---
+++ Src/ViewModels/Abstract/IViewManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Chatter.ViewModels.Abstract
+{
+    public interface IViewManager
+    {
+        Task ShowErrorBoxAsync(string message, string caption);
+    }
+}
