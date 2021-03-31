7868dc030cb744d0b1de92ef0c26988041305f83
Philip Mørch <mrphil2105@gmail.com>
2021-03-31 15:21:53 +0200

    Add window for error message box

---
+++ Src/AvaloniaApp/Views/ErrorBoxWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Chatter.AvaloniaApp.Views
+{
+    public class ErrorBoxWindow : Window
+    {
+        public ErrorBoxWindow()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
