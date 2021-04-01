33c4a7676e32daae5823e6e35a1ef49145e3cada
Philip Mørch <mrphil2105@gmail.com>
2021-04-01 12:15:48 +0200

    Add view locator for nesting views

---
+++ Src/AvaloniaApp/Views/ViewLocator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using Chatter.ViewModels;
+
+namespace Chatter.AvaloniaApp.Views
+{
+    public class ViewLocator : IDataTemplate
+    {
+        private static readonly IReadOnlyList<Type> _viewTypes = Assembly.GetExecutingAssembly()
+            .GetTypes()
+            .Where(t => t.IsClass && !t.IsAbstract && t.Name.EndsWith("View"))
+            .ToList();
+
+        public IControl Build(object viewModel)
+        {
+            string viewModelName = viewModel.GetType()
+                .Name;
+
+            if (!viewModelName.EndsWith("ViewModel"))
+            {
+                throw new ArgumentException("View model type must end with suffix 'ViewModel'.", nameof(viewModel));
+            }
+
+            int removeIndex = viewModelName.Length - "Model".Length;
+            string viewName = viewModelName.Remove(removeIndex);
+
+            var viewType = _viewTypes.SingleOrDefault(t => t.Name == viewName);
+
+            if (viewType != null)
+            {
+                return (Control)Activator.CreateInstance(viewType)!;
+            }
+
+            throw new InvalidOperationException($"View with name '{viewName}' could not be found.");
+        }
+
+        public bool Match(object data)
+        {
+            return data is ViewModelBase;
+        }
+    }
+}
