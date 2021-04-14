98b556872a264e22b2a1e7c7fa53f4870e21141c
Philip Mørch <mrphil2105@gmail.com>
2021-04-14 10:34:46 +0200

    Add IoC container and register essential services

--- Src/AvaloniaApp/App.axaml.cs
+++ Src/AvaloniaApp/App.axaml.cs
@@ -1,14 +1,42 @@
-﻿using Avalonia.Controls.ApplicationLifetimes;
+﻿using Autofac;
+using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+using Chatter.Application.Services;
+using Chatter.AvaloniaApp.Threading;
 using Chatter.AvaloniaApp.Views;
 using Chatter.ViewModels;
+using Chatter.ViewModels.Abstract;
+using IDispatcher = Chatter.ViewModels.Abstract.IDispatcher;
 
 namespace Chatter.AvaloniaApp
 {
     public class App : Avalonia.Application
     {
+        private IContainer? _container;
+
         public override void Initialize()
         {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterAssemblyTypes(typeof(IServerService).Assembly)
+                .Where(t => t.IsClass && !t.IsAbstract && t.IsInNamespace(typeof(IServerService).Namespace!))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(typeof(MainViewModel).Assembly)
+                .Where(t => t.IsClass && !t.IsAbstract && t.Name.EndsWith("ViewModel"));
+
+            builder.RegisterType<ViewManager>()
+                .As<IViewManager>()
+                .SingleInstance();
+
+            builder.Register(_ => new DispatcherWrapper(Dispatcher.UIThread))
+                .As<IDispatcher>()
+                .SingleInstance();
+
+            _container = builder.Build();
+
             AvaloniaXamlLoader.Load(this);
         }
 
