44611014112fbbba48b15238e35cd6a5f7086e2f
Philip Mørch <mrphil2105@gmail.com>
2021-04-14 11:33:07 +0200

    Add instances of view models for connection and messages

--- Src/ViewModels/MainViewModel.cs
+++ Src/ViewModels/MainViewModel.cs
@@ -1,6 +1,17 @@
-﻿namespace Chatter.ViewModels
+﻿using System;
+
+namespace Chatter.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        public MainViewModel(ConnectionViewModel connectionViewModel, MessagesViewModel messagesViewModel)
+        {
+            ConnectionViewModel = connectionViewModel ?? throw new ArgumentNullException(nameof(connectionViewModel));
+            MessagesViewModel = messagesViewModel ?? throw new ArgumentNullException(nameof(messagesViewModel));
+        }
+
+        public ConnectionViewModel ConnectionViewModel { get; }
+
+        public MessagesViewModel MessagesViewModel { get; }
     }
 }
