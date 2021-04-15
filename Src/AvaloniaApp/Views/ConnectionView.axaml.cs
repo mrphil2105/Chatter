d1e72cd9cb693920b7a1d4754276aef16c93c9cd
Philip Mørch <mrphil2105@gmail.com>
2021-04-15 12:42:23 +0200

    Add view for handling connections

---
+++ Src/AvaloniaApp/Views/ConnectionView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Chatter.AvaloniaApp.Views
+{
+    public class ConnectionView : UserControl
+    {
+        public ConnectionView()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
