1491b8503422affc6136d7d1fd8e11773aba02be
Philip Mørch <mrphil2105@gmail.com>
2021-04-15 12:43:23 +0200

    Add view for displaying, sending and receiving messages

---
+++ Src/AvaloniaApp/Views/MessageView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Chatter.AvaloniaApp.Views
+{
+    public class MessageView : UserControl
+    {
+        public MessageView()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ Src/AvaloniaApp/Views/MessagesView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Chatter.AvaloniaApp.Views
+{
+    public class MessagesView : UserControl
+    {
+        public MessagesView()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
