05c298d6a54ee17438ca3adf133a2f26ba7a2c74
Philip Mørch <mrphil2105@gmail.com>
2021-04-15 12:51:02 +0200

    Implement auto scrolling to bottom

--- Src/AvaloniaApp/Views/MessagesView.axaml.cs
+++ Src/AvaloniaApp/Views/MessagesView.axaml.cs
@@ -1,13 +1,31 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
 namespace Chatter.AvaloniaApp.Views
 {
     public class MessagesView : UserControl
     {
+        private bool _canAutoScroll;
+
         public MessagesView()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void OnScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            var scrollViewer = (ScrollViewer)sender;
+
+            // Only auto scroll if enabled and if the element has grown in height.
+            if (_canAutoScroll && e.ExtentDelta.Y > 0)
+            {
+                scrollViewer.ScrollToEnd();
+            }
+
+            // Check if the 'ScrollViewer' is scrolled to the bottom and enable auto scrolling if that is the case.
+            _canAutoScroll =
+                Math.Abs(scrollViewer.Extent.Height - (scrollViewer.Offset.Y + scrollViewer.Bounds.Height)) < 0.01;
         }
     }
 }
