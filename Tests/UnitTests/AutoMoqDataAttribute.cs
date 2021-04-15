9ee5a2fad6a9b156ee7b9874e5cbe34819946003
Philip Mørch <mrphil2105@gmail.com>
2021-04-15 21:48:49 +0200

    Create attribute for auto-mocking and auto-generating data

---
+++ Tests/UnitTests/AutoMoqDataAttribute.cs
@@ -0,0 +1,14 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+
+namespace Chatter.UnitTests
+{
+    public class AutoMoqDataAttribute : AutoDataAttribute
+    {
+        public AutoMoqDataAttribute() : base(() =>
+            new Fixture { OmitAutoProperties = true }.Customize(new AutoMoqCustomization()))
+        {
+        }
+    }
+}
